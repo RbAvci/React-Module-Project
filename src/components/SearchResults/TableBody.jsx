f0ee81fadb30b0c47f7bd64483dea0b068a446e3
Fathi Kahin <fhkahin@gmail.com>
2024-03-13 11:46:53 +0000

    feature/Error Message

--- src/components/SearchResults/TableBody.jsx
+++ src/components/SearchResults/TableBody.jsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TableBody = ({ id, title, firstName, surName, email }) => {
+  const [isSelected, setIsSelected] = useState(false); 
+
+  
+  const toggleSelection = () => {
+    setIsSelected(!isSelected);
+  };
+
+  
+  const rowStyle = {
+    backgroundColor: isSelected ? 'lightblue' : 'transparent', 
+    cursor: 'pointer' 
+  };
+
   return (
-    <tr>
+    <tr style={rowStyle} onClick={toggleSelection}>
       <td>{id}</td>
       <td>{title}</td>
       <td>{firstName}</td>
