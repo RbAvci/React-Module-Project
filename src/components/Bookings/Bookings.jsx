a40f8d3628ec46e3ae5cc52583ef6da0bf2dd5bd
Fathi Kahin <127356471+fhkahin@users.noreply.github.com>
2024-03-14 12:54:11 +0000

    Update Bookings to catch errors

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "../Search/Search";
 import FakeBookings from "../../data/fakeBookings.json";
 import SearchResults from "../SearchResults/SearchResults";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    fetchBookings();
+  }, []);
+
+  const fetchBookings = () => {
+    try {
+      // hwill replace with actual API call
+      setBookings(FakeBookings);
+    } catch (error) {
+      console.error("Error fetching bookings:", error.message);
+    }
+  };
 
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
+    // Add search functionality here
   };
 
   return (
     <main className="bookings">
       <Search search={search} />
-
       <SearchResults bookings={bookings} />
     </main>
   );
