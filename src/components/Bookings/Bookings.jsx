174c02dede3e77202413c03f4cbbebec3a056f2a
Fathi Kahin <127356471+fhkahin@users.noreply.github.com>
2024-03-19 11:29:06 +0000

    Altoms' new API used. Updated Bookings.jsx

--- src/components/Bookings/Bookings.jsx
+++ src/components/Bookings/Bookings.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "../Search/Search";
-import FakeBookings from "../../data/fakeBookings.json";
 import SearchResults from "../SearchResults/SearchResults";
 
 const Bookings = () => {
-  const [bookings, setBookings] = useState(FakeBookings);
+  const [bookings, setBookings] = useState([]);
+
+  useEffect(() => {
+    console.log("Component is mounted");
+    fetch("https://nw6-cyf-hotel.glitch.me/fakebookings")
+      .then(response => response.json())
+      .then(data => setBookings(data))
+      .catch(error => console.error("Error fetching bookings:", error));
+  }, []); 
 
   const search = (searchVal) => {
     console.info("TO DO!", searchVal);
@@ -13,7 +20,6 @@ const Bookings = () => {
   return (
     <main className="bookings">
       <Search search={search} />
-
       <SearchResults bookings={bookings} />
     </main>
   );
