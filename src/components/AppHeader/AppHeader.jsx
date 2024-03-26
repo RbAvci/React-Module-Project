33746d70cd2b4ce92b4670f39fd6e1321ec39171
RbAvci <rb.avci@gmail.com>
2024-03-26 02:55:40 +0000

    add hotel icon

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,11 +1,17 @@
 import Logo from "@/assets/hotel-logo.png";
 import "./AppHeader.scss";
+import HotelIcon from "@mui/icons-material/Hotel";
 
 const AppHeader = () => {
   return (
     <header className="app__header">
       <img className="app__logo" src={Logo} alt="hotel Logo" />
-      <h1 className="app__heading">CYF Hotel</h1>
+
+      <h1 className="app__heading">
+        {" "}
+        <HotelIcon />
+        CYF Hotel
+      </h1>
     </header>
   );
 };
