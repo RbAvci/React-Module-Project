8e96973b81b65ec42cf251ed620010faf0111683
Zeliha Pala <zelihapala78@gmail.com>
2024-04-05 23:13:48 +0100

    Implemented ascending/descending sorting for result table columns

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -1,4 +1,3 @@
-// SearchResults.jsx
 import React, { useState } from "react";
 import TableHead from "./TableHead";
 import TableBody from "./TableBody";
@@ -6,19 +5,44 @@ import CustomerProfile from "../CustomerProfile/CustomerProfile";
 
 const SearchResults = ({ bookings }) => {
   const [selectedProfileId, setSelectedProfile] = useState(null);
+  const [sortConfig, setSortConfig] = useState({
+    key: null,
+    direction: "ascending",
+  });
 
   function handleShowProfile(e) {
     setSelectedProfile(Number(e.target.value));
   }
+
+  function handleSort(key) {
+    let direction = "ascending";
+    if (sortConfig.key === key && sortConfig.direction === "ascending") {
+      direction = "descending";
+    }
+    setSortConfig({ key, direction });
+  }
+
+  const sortedBookings = [...bookings].sort((a, b) => {
+    if (sortConfig.direction === "ascending") {
+      return a[sortConfig.key] > b[sortConfig.key] ? 1 : -1;
+    } else {
+      return a[sortConfig.key] < b[sortConfig.key] ? 1 : -1;
+    }
+  });
+
   return (
     <>
       <table>
         <thead>
-          <TableHead />
+          <TableHead handleSort={handleSort} sortConfig={sortConfig} />
         </thead>
         <tbody>
-          {bookings.map((booking) => (
-            <TableBody key={booking.id} booking={booking} handleShowProfile={handleShowProfile} />
+          {sortedBookings.map((booking) => (
+            <TableBody
+              key={booking.id}
+              booking={booking}
+              handleShowProfile={handleShowProfile}
+            />
           ))}
         </tbody>
       </table>
